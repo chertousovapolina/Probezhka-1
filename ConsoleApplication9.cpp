69fef56dd4457bcaeac491142e5c8202b278e255
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-09 22:33:28 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+// Чертоусова утренняя пробежка-1
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A;
+    float B, C;
+    cin >> A >> B;
+    C = B;
+    for (int i = 0; i < A;i++) {
+        C = (C / 100) * 70;
+        C += 1;
+        
+    }
+    cout << C;
+    
+}
+
